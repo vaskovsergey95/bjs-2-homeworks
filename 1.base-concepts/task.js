b13df65ada8098e9f34dec832dbbaed7c7629072
Сергей <vaskovsergey95@gmail.com>
2023-02-01 22:39:13 +0500

    2 commit base-concept

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,20 @@
 "use strict";
 
-let decision = [];
+let arr = [];
 function solveEquation(a, b ,c) {
   let d = b ** 2 - 4* a * c;
   if (d > 0) {
     let x1 = (-b + Math.sqrt(d)) / (2 * a);
     let x2 = (-b - Math.sqrt(d)) / (2 * a);
-    decision.push(x1, x2);
+    arr.push(x1, x2);
   }
   else if (d === 0) {
     let x3 = d - b/(2*a);
-    decision.push(x3);
+    arr.push(x3);
 }
   else {
-    return decision;
+    return arr;
   }
 }
-solveEquation(3, 40, -110);
-console.log(decision)
-
-// "use strict"
-// function solveEquation(a, b, c) {
-//   let arr = [];
-//
-//   return arr;
-// }
+solveEquation(1, 5, 4);
+console.log(arr);
