39f7c0b4b1619305248041b019c2e44361828121
Сергей <vaskovsergey95@gmail.com>
2023-02-02 00:08:37 +0500

    1 correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,17 @@
 "use strict";
 
-let arr = [];
 function solveEquation(a, b ,c) {
+  let arr = [];
   let d = b ** 2 - 4* a * c;
+  let x1 = (-b + Math.sqrt(d)) / (2 * a);
+  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+  let x3 = d - b/(2*a);
   if (d > 0) {
-    let x1 = (-b + Math.sqrt(d)) / (2 * a);
-    let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
   else if (d === 0) {
-    let x3 = d - b/(2*a);
     arr.push(x3);
 }
-  else {
-    return arr;
-  }
+  return arr;
 }
-solveEquation(1, 5, 4);
-console.log(arr);
+solveEquation(1, 2, -10);
