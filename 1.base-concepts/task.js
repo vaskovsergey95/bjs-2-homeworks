fabecd75f03e3b44602c2fbb3e5dc109e5dd4feb
Сергей <vaskovsergey95@gmail.com>
2023-02-01 22:35:52 +0500

    first commit base-concept

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+let decision = [];
+function solveEquation(a, b ,c) {
+  let d = b ** 2 - 4* a * c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    decision.push(x1, x2);
+  }
+  else if (d === 0) {
+    let x3 = d - b/(2*a);
+    decision.push(x3);
 }
+  else {
+    return decision;
+  }
+}
+solveEquation(3, 40, -110);
+console.log(decision)
+
+// "use strict"
+// function solveEquation(a, b, c) {
+//   let arr = [];
+//
+//   return arr;
+// }
