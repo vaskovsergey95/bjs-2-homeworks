7b5d73eb58d487a8fb35eb7396f040e7beb374f9
Сергей <vaskovsergey95@gmail.com>
2023-02-03 23:02:32 +0500

    1 correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,13 +3,14 @@
 function solveEquation(a, b ,c) {
   let arr = [];
   let d = b ** 2 - 4* a * c;
-  let x1 = (-b + Math.sqrt(d)) / (2 * a);
-  let x2 = (-b - Math.sqrt(d)) / (2 * a);
-  let x3 = d - b/(2*a);
+
   if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
   else if (d === 0) {
+    let x3 = d - b/(2*a);
     arr.push(x3);
 }
   return arr;
