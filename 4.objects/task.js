ad7830e42a192bb374ae5747a047882f3f5fe256
vaskovsergey95 <110556385+vaskovsergey95@users.noreply.github.com>
2023-02-10 16:58:39 +0500

    Update task.js
    2commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,10 +8,6 @@ Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
-Student.prototype.addMark = function (mark) {
-  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
-}
-
 Student.prototype.addMarks = function (...marks) {
   (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
 }
